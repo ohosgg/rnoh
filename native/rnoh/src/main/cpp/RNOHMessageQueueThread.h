95fc732e81c7cdfb90da9a9a3e27a196ae010487
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-03-08 15:53:07 +0100

    refine

--- native/rnoh/src/main/cpp/RNOHMessageQueueThread.h
+++ native/rnoh/src/main/cpp/RNOHMessageQueueThread.h
@@ -1,9 +1,3 @@
-//
-// Created on 08.03.2023.
-//
-// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
-// please include "napi/native_api.h".
-
 #ifndef native_RNOHMessageQueueThread_H
 #define native_RNOHMessageQueueThread_H
 #include <cxxreact/MessageQueueThread.h>
