fce1f6bf0744f9b42290866f30a2d0912b6ca75d
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-03-08 15:23:24 +0100

    create RNOHInstance header file

--- native/rnoh/src/main/cpp/RNOHInstance.cpp
+++ native/rnoh/src/main/cpp/RNOHInstance.cpp
@@ -1,37 +1,24 @@
 #include "RNOHMessageQueueThread.h"
-#include <memory>
-#include <cxxreact/Instance.h>
-#include <cxxreact/ModuleRegistry.h>
-#include <cxxreact/NativeModule.h>
-#include <folly/dynamic.h>
-#include <utility>
-#include <thread>
+#include "RNOHInstance.h"
 
 
-class RNOHInstance {
-    RNOHInstance() 
-        instance(std::make_shared<facebook::react::Instance>())
-    {}
-    
-    public:
-    void run() {
-        this->initialize();
-        this->runApplication();
-    }
-    private:
-    std::shared_ptr<facebook::react::Instance> instance;
-    
-    void initialize() {
-        std::vector<std::unique_ptr<facebook::react::NativeModule>> modules;
-        this->instance->initializeBridge(
-                    std::make_unique<facebook::react::InstanceCallback>(),
-                    nullptr,
-                    std::make_shared<RNOHMessageQueueThread>(),
-                    std::make_shared<facebook::react::ModuleRegistry>(modules));
-    }
+void RNOHInstance::run() {
+    this->initialize();
+    this->runApplication();
+}
 
-    void runApplication() {
-        auto runAppArgs = folly::dynamic::array();
-        this->instance->callJSFunction("AppRegistry", "runApplication", std::forward<folly::dynamic>(runAppArgs));
-    }
-};
+void RNOHInstance::initialize() {
+    std::vector<std::unique_ptr<facebook::react::NativeModule>> modules;
+    this->instance->initializeBridge(
+                std::make_unique<facebook::react::InstanceCallback>(),
+                nullptr,
+                std::make_shared<RNOHMessageQueueThread>(),
+                std::make_shared<facebook::react::ModuleRegistry>(modules));
+}
+
+void RNOHInstance::runApplication() {
+    auto runAppArgs = folly::dynamic::array();
+    this->instance->callJSFunction("AppRegistry", "runApplication", std::forward<folly::dynamic>(runAppArgs));
+    this->onComponentDescriptorTreeChanged(100);
+}
+
---
+++ native/rnoh/src/main/cpp/RNOHInstance.h
@@ -0,0 +1,30 @@
+#ifndef native_RNOHInstance_H
+#define native_RNOHInstance_H
+#include <memory>
+#include <cxxreact/Instance.h>
+#include <cxxreact/ModuleRegistry.h>
+#include <cxxreact/NativeModule.h>
+#include <folly/dynamic.h>
+#include <utility>
+#include <thread>
+#include <functional>
+
+class RNOHInstance {
+    RNOHInstance(std::function<void(int)> onComponentDescriptorTreeChanged)
+        :
+        instance(std::make_shared<facebook::react::Instance>()),
+        onComponentDescriptorTreeChanged(onComponentDescriptorTreeChanged) {}
+    
+    public:
+    void run();
+    
+    private:
+    std::shared_ptr<facebook::react::Instance> instance;
+    std::function<void(int)> onComponentDescriptorTreeChanged;
+    
+    void initialize();
+    void runApplication();
+};
+
+
+#endif //native_RNOHInstance_H
