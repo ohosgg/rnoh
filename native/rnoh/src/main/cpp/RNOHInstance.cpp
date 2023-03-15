4d165dc03c52361f2db09432efe947eb28ca3ffc
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-03-15 11:52:46 +0100

    Create scheduler

--- native/rnoh/src/main/cpp/RNOHInstance.cpp
+++ native/rnoh/src/main/cpp/RNOHInstance.cpp
@@ -1,15 +1,22 @@
+#include <react/renderer/scheduler/Scheduler.h>
+#include <react/renderer/componentregistry/ComponentDescriptorRegistry.h>
 #include "RNOHMessageQueueThread.h"
 #include "RNOHInstance.h"
+#include "RNOHEventBeat.h"
 #include "hermes/executor/HermesExecutorFactory.h"
+using namespace facebook::react;
 
 void RNOHInstance::start() {
     this->initialize();
-//    this->surfaceHandler.start();
-//    this->runApplication();
+    //    this->surfaceHandler.start();
+    //    this->runApplication();
 }
 
 void RNOHInstance::initialize() {
-    std::vector<std::unique_ptr<facebook::react::NativeModule>> modules;
+    std::vector<std::unique_ptr<NativeModule>> modules;
+    this->contextContainer = std::make_shared<facebook::react::ContextContainer>();
+    this->taskExecutor = std::make_shared<RNOHTaskExecutor>();
+    this->componentDescriptorProviderRegistry = std::make_shared<facebook::react::ComponentDescriptorProviderRegistry>();
     auto instanceCallback = std::make_unique<facebook::react::InstanceCallback>();
     auto jsExecutorFactory = std::make_shared<facebook::react::HermesExecutorFactory>(
         // runtime installer, which is run when the runtime
@@ -25,6 +32,35 @@ void RNOHInstance::initialize() {
         std::move(jsExecutorFactory),
         std::move(jsQueue),
         std::move(moduleRegistry));
+}
+
+void RNOHInstance::initializeScheduler() {
+    facebook::react::EventBeat::Factory eventBeatFactory = [taskExecutor = this->taskExecutor, runtimeExecutor = this->instance->getRuntimeExecutor()](auto ownerBox) {
+        return std::make_unique<RNOHEventBeat>(taskExecutor, runtimeExecutor, ownerBox);
+    };
+
+    facebook::react::ComponentRegistryFactory componentRegistryFactory =
+        [registry = this->componentDescriptorProviderRegistry](
+            auto eventDispatcher, auto contextContainer) {
+            return registry->createComponentDescriptorRegistry(
+                {eventDispatcher, contextContainer});
+        };
+
+    auto backgroundExecutor = [executor = this->taskExecutor](std::function<void()> &&callback) {
+        executor->runOnQueue(std::move(callback));
+    };
+
+    facebook::react::SchedulerToolbox schedulerToolbox{
+        .contextContainer = this->contextContainer,
+        .componentRegistryFactory = componentRegistryFactory,
+        .runtimeExecutor = this->instance->getRuntimeExecutor(),
+        .asynchronousEventBeatFactory = eventBeatFactory,
+        .synchronousEventBeatFactory = eventBeatFactory,
+        .backgroundExecutor = backgroundExecutor,
+    };
+
+    this->schedulerDelegate = std::make_unique<RNOHSchedulerDelegate>();
+    this->scheduler = std::make_unique<facebook::react::Scheduler>(schedulerToolbox, nullptr, schedulerDelegate.get());
 }
 
 void RNOHInstance::runApplication() {
--- native/rnoh/src/main/cpp/RNOHInstance.h
+++ native/rnoh/src/main/cpp/RNOHInstance.h
@@ -8,30 +8,36 @@
 #include <utility>
 #include <thread>
 #include <functional>
-#include <react/renderer/scheduler/SurfaceHandler.h>
-
+#include <react/renderer/scheduler/Scheduler.h>
+#include <react/renderer/componentregistry/ComponentDescriptorProviderRegistry.h>
+#include "RNOHMessageQueueThread.h"
+#include "RNOHSchedulerDelegate.h"
 
 class RNOHInstance {
-    public:
+  public:
     RNOHInstance(std::function<void(int)> onComponentDescriptorTreeChanged)
-        :
-        surfaceHandler("Root", 1),
-        instance(std::make_shared<facebook::react::Instance>()),
-        onComponentDescriptorTreeChanged(onComponentDescriptorTreeChanged) {}
-    
-    
+        : surfaceHandler("Root", 1),
+          instance(std::make_shared<facebook::react::Instance>()),
+          scheduler(nullptr),
+          taskExecutor(nullptr),
+          onComponentDescriptorTreeChanged(onComponentDescriptorTreeChanged) {}
+
     void start();
     void simulateComponentDescriptorTreeUpdate();
 
-    
-    private:
+  private:
+    std::shared_ptr<facebook::react::ContextContainer> contextContainer;
     std::shared_ptr<facebook::react::Instance> instance;
     std::function<void(int)> onComponentDescriptorTreeChanged;
     facebook::react::SurfaceHandler surfaceHandler;
-    
+    std::unique_ptr<facebook::react::Scheduler> scheduler;
+    std::unique_ptr<RNOHSchedulerDelegate> schedulerDelegate;
+    std::shared_ptr<RNOHTaskExecutor> taskExecutor;
+    std::shared_ptr<ComponentDescriptorProviderRegistry> componentDescriptorProviderRegistry;
+
     void initialize();
+    void initializeScheduler();
     void runApplication();
 };
-
 
 #endif //native_RNOHInstance_H
