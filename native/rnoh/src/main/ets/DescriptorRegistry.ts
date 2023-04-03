8b9b21f969d2d24b23dd66fd1110db03a3571984
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2023-04-03 12:05:33 +0200

    Minimal component tree updates

--- native/rnoh/src/main/ets/DescriptorRegistry.ts
+++ native/rnoh/src/main/ets/DescriptorRegistry.ts
@@ -3,6 +3,7 @@ import { MutationType, Mutation } from './mutations'
 
 export class DescriptorRegistry {
   descriptorByTag: Record<Tag, Descriptor>
+  componentUpdateCallbacks: Map<Tag, (descriptor: Descriptor) => void> = new Map();
 
   constructor(descriptorByTag: Record<Tag, Descriptor>) {
     this.descriptorByTag = descriptorByTag
@@ -13,32 +14,54 @@ export class DescriptorRegistry {
   }
 
   public applyMutations(mutations: Mutation[]) {
-    mutations.forEach((mutation) => this.applyMutation(mutation))
-    // The record needs to be copied to apply updates to Ark Components.
-    this.descriptorByTag = { ...this.descriptorByTag }
+    const updatedComponents = mutations
+      .flatMap((mutation) => this.applyMutation(mutation))
+    const uniqueUpdated = [...new Set(updatedComponents)]
+    uniqueUpdated.forEach((tag) => {
+      const updatedDescriptor = this.getDescriptor(tag);
+      // NOTE: we use the spread operator here, to create a shallow
+      // copy of the descriptor object. This will cause the subscribed
+      // component to update
+      this.componentUpdateCallbacks.get(tag)?.({...updatedDescriptor})
+    })
   }
 
-  private applyMutation(mutation: Mutation) {
+  public registerComponentUpdateCallback(tag: Tag, callback: (descriptor: Descriptor) => void) {
+    this.componentUpdateCallbacks.set(tag, callback)
+
+    return () => {
+      if (this.componentUpdateCallbacks.get(tag) === callback) {
+        this.componentUpdateCallbacks.delete(tag)
+      }
+    }
+  }
+
+  private applyMutation(mutation: Mutation): Tag[] {
     console.log("[RNOH] mutation: ", JSON.stringify(mutation))
     if (mutation.type === MutationType.CREATE) {
       this.descriptorByTag[mutation.descriptor.tag] = mutation.descriptor
+      return []
     } else if (mutation.type === MutationType.INSERT) {
       const parentDescriptor = this.descriptorByTag[mutation.parentTag]
       parentDescriptor.childrenTags.push(mutation.childTag)
+      return [mutation.childTag, mutation.parentTag]
     } else if (mutation.type === MutationType.UPDATE) {
       const currentDescriptor = this.descriptorByTag[mutation.descriptor.tag]
       this.descriptorByTag[mutation.descriptor.tag] = {
         ...currentDescriptor,
         ...mutation.descriptor,
       }
+      return [mutation.descriptor.tag]
     } else if (mutation.type === MutationType.REMOVE) {
       const parentDescriptor = this.descriptorByTag[mutation.parentTag]
       const idx = parentDescriptor.childrenTags.indexOf(mutation.childTag)
       if (idx != -1) {
         parentDescriptor.childrenTags.splice(idx, 1)
       }
+      return [mutation.parentTag]
     } else if (mutation.type === MutationType.DELETE) {
       delete this.descriptorByTag[mutation.tag]
+      return []
     }
   }
 }
