8f84f8687d47935c179b53b0f7980c8cc4c43a4b
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-04-26 12:28:54 +0200

    react-native CLI

--- react-native-harmony/react-native.config.js
+++ react-native-harmony/react-native.config.js
@@ -1,28 +1,44 @@
 const fs = require("fs");
 const fse = require("fs-extra");
 const pathUtils = require("path");
+const Metro = require("metro");
 
 /**
  * @type {import("@react-native-community/cli-types").Config}
  */
 const config = {
   commands: [{
-    name: "postinstall",
+    name: "prepare-native-modules-harmony",
+    description: "Copies native modules from node_modules to the project directory.",
     func: (argv, config, args) => {
-      handlePostInstall(config.root);
+      copyHarmonyModules(config.root);
+    },
+  },
+  {
+    name: "bundle-harmony",
+    description: "Creates JS bundle, creates a special cpp header containing the JS code, copies assets directory to the project.",
+    func: async (argv, config, args) => {
+      const metroConfig = await Metro.loadConfig();
+      await Metro.runBuild(metroConfig, {
+        entry: 'index.js',
+        platform: 'harmony',
+        minify: false,
+        out: './bundle.harmony.js'
+      });
+      createHeaderFile("./bundle.harmony.js", "./harmony/entry/src/main/cpp/jsbundle.h");
+      copyAssets("./assets/", "./harmony/entry/src/main/ets/assets/assets");
     },
   }],
 };
 
 module.exports = config;
 
-function handlePostInstall(projectRootPath) {
+function copyHarmonyModules(projectRootPath) {
   forEachNodeModule(pathUtils.join(projectRootPath, "node_modules"), (path) => {
     const nodeModuleHarmonyPath = getHarmonyModulePathIfExists(path);
 
     if (nodeModuleHarmonyPath) {
-      console.log(`Copying ${path}`);
-      moveHarmonyModuleToHarmonyProject(nodeModuleHarmonyPath, pathUtils.join(projectRootPath, "harmony"));
+      copyHarmonyModuleToHarmonyProject(projectRootPath, nodeModuleHarmonyPath, pathUtils.join(projectRootPath, "harmony"));
     }
   });
 }
@@ -43,6 +59,29 @@ function getHarmonyModulePathIfExists(nodeModulePath) {
   }
 }
 
-function moveHarmonyModuleToHarmonyProject(harmonyModulePath, harmonyProjectPath) {
-  fse.copySync(harmonyModulePath, pathUtils.join(harmonyProjectPath, "react_native_modules"), { overwrite: true });
+function copyHarmonyModuleToHarmonyProject(projectRootPath, harmonyModulePath, harmonyProjectPath) {
+  const outputDirPath = pathUtils.join(harmonyProjectPath, "react_native_modules");
+  fse.copySync(harmonyModulePath, outputDirPath, { overwrite: true });
+  fs.readdirSync(harmonyModulePath).forEach((filePath) => {
+    console.log(`[CREATED] ${pathUtils.relative(projectRootPath, pathUtils.join(outputDirPath, filePath))}`);
+  });
 }
+
+function copyAssets(srcPath, destPath) {
+  fse.copySync(srcPath, destPath, { overwrite: true });
+  console.log(`[CREATED] ${destPath}`);
+}
+
+function createHeaderFile(jsBundlePath, headerFilePath) {
+  const jsBundle = fs.readFileSync(jsBundlePath, 'utf8');
+  const content = generateHeaderContent(jsBundle);
+  fs.writeFileSync(headerFilePath, content, 'utf8');
+  console.log(`[CREATED] ${headerFilePath}`);
+};
+
+function generateHeaderContent(jsBundle) {
+  return `#pragma once
+#include <string>
+
+constexpr const char *JS_BUNDLE = R"JSBUNDLE(${jsBundle})JSBUNDLE";`;
+};
