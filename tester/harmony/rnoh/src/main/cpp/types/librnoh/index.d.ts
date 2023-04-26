0ef166f33cf14e8d12462509a237e43695c1945c
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-04-26 09:55:28 +0200

    update react-native config and remove types for librnoh

--- tester/harmony/rnoh/src/main/cpp/types/librnoh/index.d.ts
+++
@@ -1,5 +0,0 @@
-export const subscribeToShadowTreeChanges: (
-  onShadowTreeChange: (mutations: any[]) => void
-) => number
-export const startReactNative: (width: number, height: number) => void
-export const initializeReactNative: () => void
