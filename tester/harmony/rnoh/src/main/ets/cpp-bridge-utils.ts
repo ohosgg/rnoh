1d0d317a9e47c9ff5c0d4af3a9899c73bcfc123c
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-05-04 12:00:11 +0200

    ColorValue converter. Use barrel file.

--- tester/harmony/rnoh/src/main/ets/cpp-bridge-utils.ts
+++ tester/harmony/rnoh/src/main/ets/cpp-bridge-utils.ts
@@ -7,3 +7,13 @@ export function convertColorSegmentsToString(colorSegments?: ColorSegments) {
     b * 255
   )}, ${a})`
 }
+
+export function convertColorValueToRGBA(colorValue: number) {
+  const rgba = {
+    r: (colorValue >> 24) & 0xff,
+    g: (colorValue >> 16) & 0xff,
+    b: (colorValue >> 8) & 0xff,
+    a: ((colorValue >> 0) & 0xff) / 255,
+  }
+  return `rgba(${rgba.r}, ${rgba.g}, ${rgba.b}, ${rgba.a})`
+}
