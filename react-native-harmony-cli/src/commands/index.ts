53af1cd19705a4195356ec6ba67c37deb79e1627
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-05-25 18:29:20 +0200

    remove old prepare-harmony-modules command

--- react-native-harmony-cli/src/commands/index.ts
+++ react-native-harmony-cli/src/commands/index.ts
@@ -1,4 +1,3 @@
 export * from './bundle-harmony';
-export * from './prepare-native-modules-harmony';
 export * from './pack-harmony';
 export * from './unpack-harmony';
--- react-native-harmony-cli/src/index.ts
+++ react-native-harmony-cli/src/index.ts
@@ -1,16 +1,10 @@
 import { Config } from '@react-native-community/cli-types';
 import {
   commandBundleHarmony,
-  commandPrepareNativeModulesHarmony,
   commandPackHarmony,
   commandUnpackHarmony,
 } from './commands';
 
 export const config = {
-  commands: [
-    commandPrepareNativeModulesHarmony,
-    commandBundleHarmony,
-    commandPackHarmony,
-    commandUnpackHarmony,
-  ],
+  commands: [commandBundleHarmony, commandPackHarmony, commandUnpackHarmony],
 } satisfies Partial<Config>;
