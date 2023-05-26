f6931a4cba34784e6cd56a7928097def8cda58b9
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-05-26 15:14:40 +0200

    Animated.ScrollView test

--- tester/tests/AnimatedTest.tsx
+++ tester/tests/AnimatedTest.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-import {Animated} from 'react-native';
+import {Animated, View} from 'react-native';
 
 import {TestCase, TestSuite} from '@rnoh/testerino';
 
@@ -9,6 +9,9 @@ export function AnimatedTest() {
     <TestSuite name="Animated">
       <TestCase itShould="animate width">
         <AnimatedRectangle />
+      </TestCase>
+      <TestCase itShould="move red square horizontally relatively to the scroll offset">
+        <AnimatedScrollViewTestCase />
       </TestCase>
     </TestSuite>
   );
@@ -45,3 +48,59 @@ function AnimatedRectangle() {
     />
   );
 }
+
+const AnimatedScrollViewTestCase = () => {
+  const scrollY = new Animated.Value(0);
+  const translation = scrollY.interpolate({
+    inputRange: [0, 200],
+    outputRange: [0, 200],
+    extrapolate: 'clamp',
+  });
+
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: 100,
+        position: 'relative',
+        overflow: 'hidden',
+      }}>
+      <Animated.ScrollView
+        style={{width: '100%', height: '100%'}}
+        contentContainerStyle={{alignItems: 'center', justifyContent: 'center'}}
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {y: scrollY}}}],
+          {
+            useNativeDriver: false,
+          },
+        )}>
+        {new Array(3).fill(0).map((_, idx) => {
+          return (
+            <View
+              key={idx}
+              style={{
+                width: '100%',
+                height: 50,
+                backgroundColor: 'gray',
+                marginBottom: 50,
+              }}
+            />
+          );
+        })}
+      </Animated.ScrollView>
+      <Animated.View
+        style={[
+          {
+            position: 'absolute',
+            bottom: 0,
+            left: translation,
+            width: 32,
+            height: 32,
+            backgroundColor: 'red',
+          },
+        ]}
+      />
+    </View>
+  );
+};
