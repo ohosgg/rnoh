595fa38f310e22beb952515e9881a43520d2ebc6
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-05-31 14:59:40 +0200

    Update animation examples

--- tester/examples/AnimationsExample.tsx
+++ tester/examples/AnimationsExample.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import {Animated, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Animated,
+  ScrollView,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export function AnimationsExample() {
+  const [isIntervalRunning, setIsIntervalRunning] = useState(false);
+  const [isTogglingComponent, setIsTogglingComponent] = useState(false);
+  const [isComponentVisible, setIsComponentVisible] = useState(false);
   const animWidth = React.useRef(new Animated.Value(100)).current;
+  const fadeAnim = React.useRef(new Animated.Value(1)).current;
 
+  const handleFadePress = () => {
+    Animated.sequence([
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
   const animation = React.useMemo(() => {
     const expand = Animated.timing(animWidth, {
       toValue: 300,
@@ -18,18 +43,78 @@ export function AnimationsExample() {
     return Animated.sequence([expand, contract]);
   }, []);
 
+  useEffect(() => {
+    const timerId = isIntervalRunning
+      ? setInterval(() => {
+          setTimeout(() => {}, 1);
+        }, 1)
+      : undefined;
+    return () => {
+      if (timerId) clearInterval(timerId);
+    };
+  }, [isIntervalRunning]);
+
+  useEffect(() => {
+    let timerId: any;
+    if (isTogglingComponent)
+      timerId = setInterval(() => {
+        setIsComponentVisible(prev => !prev);
+      }, 1000);
+    return () => {
+      if (timerId) {
+        clearInterval(timerId);
+      }
+    };
+  }, [isTogglingComponent]);
+
   return (
-    <Animated.View
-      style={{
-        height: 100,
-        width: animWidth,
-        backgroundColor: 'red',
-      }}
-      onTouchEnd={() => {
-        animation.reset();
-        animation.start();
-      }}>
-      <Text style={{width: 100, height: 24}}>Press me</Text>
-    </Animated.View>
+    <ScrollView contentContainerStyle={{height: 2000}}>
+      <TouchableOpacity
+        onPress={() => {
+          setIsIntervalRunning(prev => !prev);
+        }}>
+        <Text style={{width: '100%', height: 32}}>
+          {isIntervalRunning ? 'Stop running interval' : 'Run interval'}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          setIsTogglingComponent(prev => !prev);
+        }}>
+        <Text style={{width: '100%', height: 32}}>
+          {isTogglingComponent
+            ? 'Stop toggling component'
+            : 'Start toggling component'}
+        </Text>
+      </TouchableOpacity>
+
+      <Animated.View
+        style={{
+          height: 100,
+          width: animWidth,
+          backgroundColor: 'red',
+        }}
+        onTouchEnd={() => {
+          animation.reset();
+          animation.start();
+        }}>
+        <Text style={{width: 100, height: 24}}>Press me</Text>
+      </Animated.View>
+      <Animated.View
+        style={{
+          marginTop: 24,
+          height: 100,
+          width: 100,
+          opacity: fadeAnim,
+          backgroundColor: 'red',
+        }}
+        onTouchEnd={handleFadePress}>
+        <Text style={{width: 100, height: 48}}>Press me to fade</Text>
+      </Animated.View>
+
+      {isComponentVisible && (
+        <View style={{width: 100, height: 100, backgroundColor: 'blue'}} />
+      )}
+    </ScrollView>
   );
 }
