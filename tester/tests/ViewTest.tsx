ba1324201da1cdbcb4277510acaabe1ea642e226
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-06-13 18:08:44 +0200

    add ref.measure test

--- tester/tests/ViewTest.tsx
+++ tester/tests/ViewTest.tsx
@@ -1,5 +1,6 @@
-import {View} from 'react-native';
+import {Text, View, UIManager} from 'react-native';
 import {TestSuite, TestCase} from '@rnoh/testerino';
+import React, {useEffect} from 'react';
 
 export function ViewTest() {
   return (
@@ -16,6 +17,32 @@ export function ViewTest() {
           />
         </View>
       </TestCase>
+      <TestCase itShould="display component dimensions">
+        <View style={{width: '100%', height: 96}}>
+          <DimensionsDisplayer />
+        </View>
+      </TestCase>
     </TestSuite>
+  );
+}
+
+function DimensionsDisplayer() {
+  const [dimensions, setDimensions] = React.useState({width: 0, height: 0});
+  const viewRef = React.useRef<View>(null);
+
+  useEffect(() => {
+    viewRef.current?.measure((_x, _y, width, height) => {
+      setDimensions({width, height});
+    });
+  }, []);
+
+  return (
+    <View
+      ref={viewRef}
+      style={{backgroundColor: 'blue', width: 96, height: 32}}>
+      <Text style={{width: '100%', height: '100%', color: 'white'}}>
+        width: {dimensions.width} height: {dimensions.height}
+      </Text>
+    </View>
   );
 }
