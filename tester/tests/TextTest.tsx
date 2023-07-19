f4ebb0ffd7c798ec3b4cf23a0157aea5973fc66b
Przemysław Sosna <przemyslaw.sosna@swmansion.com>
2023-07-19 17:39:38 +0200

    add custom font test

--- tester/tests/TextTest.tsx
+++ tester/tests/TextTest.tsx
@@ -11,6 +11,18 @@ export function TextTest() {
           <Text style={{height: '100%', backgroundColor: 'pink'}}>BAR</Text>
         </View>
       </TestCase>
+      <TestCase itShould="[FAILS] show text with the dancing script font ">
+        <View style={{height: 30, width: '100%'}}>
+          <Text
+            style={{
+              ...styles.blackText,
+              fontFamily: 'DancingScript-Regular',
+              padding: 5,
+            }}>
+            Lorem ipsum dolor sit amet
+          </Text>
+        </View>
+      </TestCase>
       <TestCase itShould="show text with different alignments">
         <View style={styles.container}>
           <Text style={styles.text}>Left</Text>
@@ -22,9 +34,7 @@ export function TextTest() {
         <View style={styles.container}>
           <Text style={[styles.text, {textAlign: 'right'}]}>
             <Text style={{fontWeight: 'bold'}}>Bold</Text>
-            <Text style={{fontStyle: 'italic'}}>
-              Italic
-            </Text>
+            <Text style={{fontStyle: 'italic'}}>Italic</Text>
           </Text>
         </View>
       </TestCase>
@@ -41,5 +51,10 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '33%',
     color: 'white',
+  },
+  blackText: {
+    width: '100%',
+    height: '100%',
+    color: 'black',
   },
 });
