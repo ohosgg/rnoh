3d4d88c6d824abf3c11ebfb1922f75b56688b137
Zofia Glapa <zofia.glapa@swmansion.com>
2023-08-28 09:44:01 +0200

    Make rn bundle-harmony similar to rn bundle

--- react-native-harmony-cli/src/commands/bundle-harmony.ts
+++ react-native-harmony-cli/src/commands/bundle-harmony.ts
@@ -4,6 +4,7 @@ import fse from 'fs-extra';
 import Metro from 'metro';
 
 const ARK_RESOURCE_PATH = './harmony/entry/src/main/resources/rawfile';
+const ASSETS_DEFAULT_DEST_PATH = './harmony/entry/src/main/ets/assets/assets';
 
 export const commandBundleHarmony: Command = {
   name: 'bundle-harmony',
@@ -11,8 +12,9 @@ export const commandBundleHarmony: Command = {
     'Creates JS bundle, creates a special cpp header containing the JS code, copies assets directory to the project.',
   options: [
     {
-      name: '--prod',
-      description: 'Warnings are disabled and the bundle is minified',
+      name: '--dev',
+      description: 'If false, warnings are disabled and the bundle is minified',
+      default: 'true',
     },
     {
       name: '--entry-file <path>',
@@ -20,20 +22,55 @@ export const commandBundleHarmony: Command = {
         'Path to the root JS file, either absolute or relative to JS root',
       default: 'index.js',
     },
+    {
+      name: '--config <path>',
+      description: 'Path to the Metro configuration file',
+    },
+    {
+      name: '--bundle-output <path>',
+      description: 'File name where to store the resulting bundle, ex. /tmp/groups.bundle',
+      default: ARK_RESOURCE_PATH + '/bundle.harmony.js',
+    },
+    {
+      name: '--assets-dest <path>',
+      description: 'Directory name where to store assets referenced in the bundle',
+      default: ASSETS_DEFAULT_DEST_PATH,
+    },
+    {
+      name: '--sourcemap-output <path>',
+      description: 'File name where to store the resulting source map, ex. /tmp/groups.map',
+    },
   ],
   func: async (argv, config, args: any) => {
-    const metroConfig = await Metro.loadConfig();
-    await fse.ensureDir(ARK_RESOURCE_PATH);
-    await Metro.runBuild(metroConfig, {
+    const metroConfig = (args.config ) ? await Metro.loadConfig(args.config) : await Metro.loadConfig();
+    if (args.bundleOutput === ARK_RESOURCE_PATH + '/bundle.harmony.js') {
+      await fse.ensureDir(ARK_RESOURCE_PATH);
+    }
+
+    // casting needed as Metro.runBuild returns Promise<{code: string, map: string}> 
+    // despite being typed as Promise<void> 
+    ((Metro.runBuild(metroConfig, {
       entry: args.entryFile,
       platform: 'harmony',
       minify: false,
-      out: ARK_RESOURCE_PATH + '/bundle.harmony.js',
-      dev: args.prod ?? true,
+      dev: args.dev ?? true,    
+      sourceMap: args.sourcemapOutput,
+    }) as unknown) as Promise<{code: string, map:string}>).then((bundle) => {
+      saveBundle(bundle, args.bundleOutput, args.sourcemapOutput)
+      copyAssets('./assets/', args.assetsDest);
     });
-    copyAssets('./assets/', './harmony/entry/src/main/ets/assets/assets');
   },
 };
+
+function saveBundle(bundle: {code: string, map: string}, bundleOutput: string, sourceMapOutput: string | undefined) {
+    fs.writeFileSync(bundleOutput, bundle.code);
+    console.log(`[CREATED] ${bundleOutput}`);
+
+    if (sourceMapOutput) {
+      fs.writeFileSync(sourceMapOutput, bundle.map);
+      console.log(`[CREATED] ${sourceMapOutput}`);
+    }
+} 
 
 function copyAssets(srcPath: string, destPath: string) {
   if (fs.existsSync(srcPath)) {
