582b38d848b0b5bc32e88721a55653e4c01beabd
yilinlin <yilinlin@email.com>
2023-10-17 10:40:40 +0800

    fix:The third-party library cannot obtain the manager

--- tester/harmony/rnoh/src/main/ets/RNOHCorePackage/ts.ts
+++ tester/harmony/rnoh/src/main/ets/RNOHCorePackage/ts.ts
@@ -2,3 +2,4 @@ export * from "./Package";
 export type { DeviceInfoTurboModule } from "./turboModules/DeviceInfoTurboModule";
 export * from "./RNOHEventEmitRequestHandler"
 export * from './components/ts'
+export * from './componentManagers'
