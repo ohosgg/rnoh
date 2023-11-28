8e3db4493f04229d3357600746c75e7eff306df2
Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>
2023-11-28 09:21:21 +0100

    Convert ReactNativeVersion to the same form as IOS returns
    See merge request rnoh/react-native-harmony!586

--- tester/harmony/rnoh/src/main/ets/RNOHCorePackage/turboModules/PlatformConstantsTurboModule.ts
+++ tester/harmony/rnoh/src/main/ets/RNOHCorePackage/turboModules/PlatformConstantsTurboModule.ts
@@ -5,12 +5,18 @@ export class PlatformConstantsTurboModule extends TurboModule {
   public static readonly NAME = 'PlatformConstants';
 
   getConstants() {
+    const versionParts = this.ctx.reactNativeVersion.split('.', 3);
+
     return {
       Model: deviceInfo.productModel,
       deviceType: deviceInfo.deviceType,
       osFullName: deviceInfo.osFullName,
       isTesting: false,
-      reactNativeVersion: this.ctx.reactNativeVersion
+      reactNativeVersion: {
+        major: versionParts[0],
+        minor: versionParts[1],
+        patch: versionParts[2],
+      }
     };
   }
 }
